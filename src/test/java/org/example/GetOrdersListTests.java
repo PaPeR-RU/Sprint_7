0895306222a38bfb88f48084897752f825bbfb71
Dmitriy Zavgorodniy <d1ma.zav@yandex.ru>
2025-02-12 20:22:48 +0300

    Attempt #1

---
+++ src/test/java/org/example/GetOrdersListTests.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.qameta.allure.*;
+import io.qameta.allure.junit4.DisplayName;
+import io.qameta.allure.junit4.Tag;
+import io.restassured.response.Response;
+import org.example.resthandlers.apihandlers.OrdersAPIHandler;
+import org.junit.Test;
+
+@Link(url = "https://qa-scooter.praktikum-services.ru/docs/#api-Orders-CreateOrder", name = "#api-Orders-GetOrdersPageByPage")
+@Tag("get-orders-list")
+@Epic("Sprint 7. Project")
+@Feature("Группа тестов для API получения списка заказа")
+@DisplayName("4. Получение списка заказов")
+public class GetOrdersListTests extends OrdersAPIHandler {
+
+    @Test
+    @DisplayName("Получение списка заказов")
+    @Description("Тест проверки API на получение списка заказов. ОР - возвращается список заказов")
+    public void getOrderListWithoutParamsIsSuccess() {
+        Response response = getOrdersList();
+
+        checkStatusCode(response, 200);
+        checkOrdersInResponse(response);
+    }
+}
